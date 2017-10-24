d6d69c7a09ca9b27936d4ac17bd6e43d934a708f
ADRIANO MARINHO DE MELO <adriano.de.melo@accenture.com>
2017-10-24 14:20:47 -0200

    Adicionado funcionalidades para: Ir para site, fazer ligação, enviar sms, localizar no mapa

--- app/src/main/java/br/com/adrianodemelo/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/adrianodemelo/agenda/ListaAlunosActivity.java
@@ -1,6 +1,10 @@
 package br.com.adrianodemelo.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -45,9 +49,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 Intent intentFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
                 intentFormulario.putExtra("aluno", aluno);
                 startActivity(intentFormulario);
-
-                //Toast.makeText(ListaAlunosActivity.this, "Aluno selecionado: " + aluno.getNome(), Toast.LENGTH_SHORT).show();
-
             }
         });
 
@@ -62,13 +63,50 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
-        MenuItem deletar = menu.add("Deletar");
-        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) lista_alunos.getItemAtPosition(info.position);
+
+        MenuItem itemSite = menu.add("Ir Para Site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        String site = aluno.getSite();
+        if (!site.startsWith("http://"))
+            site = "http://" + site;
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
+        MenuItem itemSMS = menu.add("SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        final MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) lista_alunos.getItemAtPosition(info.position);
 
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED)
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,new String[]{Manifest.permission.CALL_PHONE}, 123);
+                else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+
+                    startActivity(intentLigar);
+                }
+                return false;
+                
+            }
+        });
+
+        MenuItem itemMapa = menu.add("Vai para o Mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?z=14&q="+aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+        MenuItem itemDeletar = menu.add("Deletar");
+        itemDeletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
                 dao.close();
